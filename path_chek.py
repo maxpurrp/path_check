09ea4a36b3d4d74b1d81df3c3f66bfec907af421
maxpurrp <seminm041@gmail.com>
2023-03-11 16:57:11 -0500

    add if

--- path_chek.py
+++ path_chek.py
@@ -1,18 +1,18 @@
 import os
 import argparse
-
-def show_dir(path):
-    global tab_for_files, tab_for_dir
-    for q in os.listdir(path):
-        if os.path.isdir(os.path.join(path, q)):
-            tab_for_dir += 1
-            print("    "* tab_for_dir, os.path.basename(q))
-            tab_for_files += 1
-            show_dir(os.path.join(path,q))
-            tab_for_files -= 1
-            tab_for_dir -= 1
-        else:
-            print("    " * tab_for_files, os.path.basename(q), ">", os.path.getsize(os.path.join(path,q)) / 1024 / 1024, "Mb")
+if __name__ == "__main__":
+    def show_dir(path):
+        global tab_for_files, tab_for_dir
+        for q in os.listdir(path):
+            if os.path.isdir(os.path.join(path, q)):
+                tab_for_dir += 1
+                print("    "* tab_for_dir, os.path.basename(q))
+                tab_for_files += 1
+                show_dir(os.path.join(path,q))
+                tab_for_files -= 1
+                tab_for_dir -= 1
+            else:
+                print("    " * tab_for_files, os.path.basename(q), ">", os.path.getsize(os.path.join(path,q)) / 1024 / 1024, "Mb")
 
 tab_for_files = 1
 tab_for_dir = 0
