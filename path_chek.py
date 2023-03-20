665a05dff77715cf9b711bb8de425e5c971a2b48
maxpurrp <seminm041@gmail.com>
2023-03-20 16:40:55 -0400

    fix and update

--- path_chek.py
+++ path_chek.py
@@ -1,33 +1,29 @@
 import os
 import argparse
 
-def size_type(path, q):
-    if args.s_t == "Kb":
-        x = os.path.getsize(os.path.join(path,q)) / 1024
-        return str(x) + " Kb"
-    elif args.s_t == "Mb":
-        x = os.path.getsize(os.path.join(path,q)) / 1024 / 1024
-        return str(x) + " Mb"
-    elif args.s_t == "Gb":
-        x = os.path.getsize(os.path.join(path,q)) / 1024 / 1024 / 1024
-        return str(x) + " Gb"
+def size_type(path, file, size_info):
+    table = {}
+    table["Kb"] = os.path.getsize(os.path.join(path,file)) / 1024
+    table["Mb"] = os.path.getsize(os.path.join(path,file)) / 1024 / 1024
+    table["Gb"] = os.path.getsize(os.path.join(path,file)) / 1024 / 1024 / 1024
+    return str(table[size_info]) + size_info
 
-def show_dir(path):
+def show_dir(path, size_info):
     global tab_for_lines
     try:
-        for q in os.listdir(path):
-            if os.path.isdir(os.path.join(path, q)):
-                print("    "* tab_for_lines, os.path.basename(q))
+        for file in os.listdir(path):
+            if os.path.isdir(os.path.join(path, file)):
+                print("    "* tab_for_lines, os.path.basename(file))
                 tab_for_lines += 1
-                show_dir(os.path.join(path,q))
+                show_dir(os.path.join(path,file), size_info)
                 tab_for_lines -= 1
             else:
                 try:
-                    print("    " * tab_for_lines, os.path.basename(q), ">", size_type(path, q))
+                    print("    " * tab_for_lines, os.path.basename(file), ">", size_type(path, file, size_info))
                 except FileNotFoundError:
-                    print("    " * tab_for_lines, os.path.basename(q) ,"FileNotFound")
+                    print("    " * tab_for_lines, os.path.basename(file) ,"FileNotFound")
                 except PermissionError:
-                    print("    " * tab_for_lines, os.path.basename(q) ,"Access Denied")
+                    print("    " * tab_for_lines, os.path.basename(file) ,"Access Denied")
 
     except FileNotFoundError:
         print("    "* tab_for_lines, path, "DirectoryNotFound")
@@ -42,4 +38,6 @@ if __name__ == "__main__":
     parser.add_argument("-path", "--PATH",required=True)
     parser.add_argument("--s_t","--size_type", default = "Mb", choices=["Kb", "Mb", "Gb"])
     args = parser.parse_args()
-    show_dir(args.PATH)
+    size_info = ""
+    size_info += args.s_t
+    show_dir(args.PATH, size_info)
