34f6854103a5ec5d8dfa92df8481749c4942c631
maxpurrp <seminm041@gmail.com>
2023-04-18 10:30:21 -0400

    add class prefix and rename variable

--- path_chek.py
+++ path_chek.py
@@ -1,39 +1,56 @@
 import os
 import argparse
 
-def show_dir(path):
-    global lvl_in
+class MyPrefix(list):
+    def __repr__(self) -> str:
+        res = ""
+        for i in range(len(self)):
+            res += str(self.__getitem__(i))
+        return res
+    def __str__(self) -> str:
+        res = ""
+        for i in range(len(self)):
+            res += str(self.__getitem__(i))
+        return res
+    
+line_to_obj = "|----"
+empty_line =  "     "
+slash_line =  "|    "
+
+def show_dir(path,prefix):
     try:
-        for q in os.listdir(path):
-            if os.path.isdir(os.path.join(path, q)):
-                if lvl_in == 0:
-                    print(("|"+ ("     ")* lvl_in) + "----" +  os.path.basename(q))
-                else:
-                    print(("|"+ "     "* lvl_in) +"|" + "----" +  os.path.basename(q))
-                lvl_in += 1
-                show_dir(os.path.join(path,q))
-                lvl_in -= 1
-            else:
-                try:
-                    if lvl_in == 0:
-                        print("|"+ "     "* lvl_in + "----" + os.path.basename(q) + ">", os.path.getsize(os.path.join(path,q)) / 1024 / 1024, "Mb")
+        dirlist = os.listdir(path)
+        for file in range (len(dirlist)):
+            if os.path.isdir(os.path.join(path, dirlist[file])):   
+                    prefix.append(line_to_obj)
+                    print(str(prefix) + os.path.basename(dirlist[file]))
+                    prefix.pop()                  
+                    if file == len(dirlist) - 1:
+                        prefix.append(empty_line)
                     else:
-                        print("|"+ "     "* lvl_in + "|----" + os.path.basename(q) + ">", os.path.getsize(os.path.join(path,q)) / 1024 / 1024, "Mb")
+                        prefix.append(slash_line)
+                    show_dir(os.path.join(path,dirlist[file]),prefix)
+                    prefix.pop()
+
+            else:
+                prefix.append(line_to_obj)
+                try:                    
+                    print(str(prefix) +  os.path.basename(dirlist[file]) + "  " + str(os.path.getsize(os.path.join(path,dirlist[file])) / 1024 / 1024), "Mb")
                 except FileNotFoundError:
-                    print("|"+ "     "* lvl_in + os.path.basename(q) + ">", "fileNotFound")
+                    print(str(prefix) + os.path.basename(dirlist[file]) +" FileNotFound")
                 except PermissionError:
-                    print("|"+ "     "* lvl_in + os.path.basename(q) + ">", "PermissionDenied")
+                    print(str(prefix) + os.path.basename(dirlist[file]) + " PermissionDenied")
+                prefix.pop()
 
     except FileNotFoundError:
-        print(("|"+ "     "* lvl_in) + "----", path, "DirectoryNotFound")
+        print(str(prefix) + path, "DirectoryNotFound")
     except PermissionError:
-        print(("|"+ "     "* lvl_in) + "----", path, "Access Denied")       
+        print(str(prefix) + path, "Access Denied")       
     except OSError:
         pass
-
 if __name__ == "__main__":
-    lvl_in = 0
     parser = argparse.ArgumentParser()
     parser.add_argument("-path","--PATH",required=True)
     args = parser.parse_args()
-    show_dir(args.PATH)
+    print(args.PATH)
+    show_dir(args.PATH, MyPrefix())
